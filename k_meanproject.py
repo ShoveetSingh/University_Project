0fc59e4e20115455f5e33ef1cbba1f1d819b40cb
@Shoveet2003 <shoveetsingh2002@gmail.com>
2025-01-30 20:42:18 +0530

    Final Commit

--- k_meanproject.py
+++ k_meanproject.py
@@ -7,22 +7,24 @@ import pandas as pd
 data = pd.read_csv('College_Data',index_col=0)
 print(data)
 
-# sns.scatterplot(x="Grad.Rate",y="Room.Board",data=data,hue="Private")
-# plt.show()
-# sns.scatterplot(x="F.Undergrad",y="Outstate",data=data,hue="Private")
-# plt.show()
-# sns.set_style('darkgrid')
-# g=sns.FacetGrid(data,hue="Private",palette='coolwarm',height=6,aspect=2)
-# g.map(plt.hist,"Outstate",bins=20,alpha=0.7)
-# plt.show()
+sns.scatterplot(x="Grad.Rate",y="Room.Board",data=data,hue="Private")
+plt.show()
+sns.scatterplot(x="F.Undergrad",y="Outstate",data=data,hue="Private")
+plt.show()
+sns.set_style('darkgrid')
+g=sns.FacetGrid(data,hue="Private",palette='coolwarm',height=6,aspect=2)
+g.map(plt.hist,"Outstate",bins=20,alpha=0.7)
+plt.show()
 
-# sns.set_style('darkgrid')
-# g= sns.FacetGrid(data,hue="Private",palette="coolwarm",height=6,aspect=2)
-# g.map(plt.hist,"Grad.Rate",bins=20,alpha=0.8)
-# plt.show()
+sns.set_style('darkgrid')
+g= sns.FacetGrid(data,hue="Private",palette="coolwarm",height=6,aspect=2)
+g.map(plt.hist,"Grad.Rate",bins=20,alpha=0.8)
+plt.show()
 
-#print()
+print(data[data["Grad.Rate"]>100])
 data.loc['Cazenovia College','Grad.Rate']=90
+
+
 sns.set_style('darkgrid')
 g= sns.FacetGrid(data,hue="Private",palette="coolwarm",height=6,aspect=2)
 g.map(plt.hist,"Grad.Rate",bins=20,alpha=0.8)
@@ -54,9 +56,14 @@ print(data["Cluster"])
 
 # data["Cluster"]=df
 
-f,(ax1,ax2)=plt.subplots(2,1, sharey=True,figsize=(10,6))
-ax1.scatter(data["Accept"],data["Enroll"],c=predict,cmap='rainbow')
+f,(ax1,ax2)=plt.subplots(2,1,sharey=True,figsize=(10,6))
+ax1.scatter(data["PhD"],data["Outstate"],c=predict,cmap='rainbow')
 ax2.scatter(data["Accept"],data["Enroll"],c=data["Cluster"],cmap='rainbow')
 ax1.set_title("Predicted")
 ax2.set_title("Original")
 plt.show()
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+print(confusion_matrix(data["Cluster"],predict))
+print(classification_report(data["Cluster"],predict))
